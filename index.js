b3940e4eb6ba3f00594f2d234ba3b2bceb4d8068
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-06 01:44:45 -0400

    use caporal library

--- index.js
+++ index.js
@@ -4,15 +4,26 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
 
-//* Wait for 100ms after chokidar finishes event
-const start = debounce(() => {
-  console.log('STARTING USERS PROGRAM');
-}, 100);
+//* Framework for command line applications and auto-generated help
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action((args) => {
+    console.log(args)
+  });
 
-//* chokidar watches current directory for changes with events
-chokidar
-  .watch('.')
-  .on('add', () => start)
-  .on('change', () => console.log('File Changed'))
-  .on('unlink', () => console.log('File Unlinked'));
+program.parse(process.argv)
+
+// //* Wait for 100ms after chokidar finishes event
+// const start = debounce(() => {
+//   console.log('STARTING USERS PROGRAM');
+// }, 100);
+
+// //* chokidar watches current directory for changes with events
+// chokidar
+//   .watch('.')
+//   .on('add', () => start)
+//   .on('change', () => console.log('File Changed'))
+//   .on('unlink', () => console.log('File Unlinked'));
