3c59c2c1f3b8ed2e8478317f027c915868edd526
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-06 01:01:50 -0400

    added chokidar

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 //* ^--Code run in Node
-// Developer Note: npm link -> run filename
+// Developer Note for new commands: npm link -> run filename
 
-console.log('Hi Message');
+const chokidar = require('chokidar');
+
+//* Watch current directory for changes
+chokidar
+  .watch('.')
+  .on('add', () => console.log('File Added'))
+  .on('change', () => console.log('File Changed'))
+  .on('unlink', () => console.log('File Unlinked'));
