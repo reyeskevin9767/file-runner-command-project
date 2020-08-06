7e268a4bea9496723dbea61d98ff05198b67014e
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-06 01:56:55 -0400

    use fs to check if file exists

--- index.js
+++ index.js
@@ -5,25 +5,33 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
 
 //* Framework for command line applications and auto-generated help
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of a file to execute')
-  .action((args) => {
-    console.log(args)
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    // Wait for 100ms after chokidar finishes event
+    const start = debounce(() => {
+      console.log('STARTING USERS PROGRAM');
+    }, 100);
+
+    // chokidar watches current directory for changes with events
+    chokidar
+      .watch('.')
+      .on('add', () => start)
+      .on('change', () => start)
+      .on('unlink', () => start);
+    console.log(args);
   });
 
-program.parse(process.argv)
-
-// //* Wait for 100ms after chokidar finishes event
-// const start = debounce(() => {
-//   console.log('STARTING USERS PROGRAM');
-// }, 100);
-
-// //* chokidar watches current directory for changes with events
-// chokidar
-//   .watch('.')
-//   .on('add', () => start)
-//   .on('change', () => console.log('File Changed'))
-//   .on('unlink', () => console.log('File Unlinked'));
+program.parse(process.argv);
