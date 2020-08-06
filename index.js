b38b72018136d054795711be3634316f737ffe42
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-06 01:18:07 -0400

    add debounce function to chokidar

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 //* ^--Code run in Node
 // Developer Note for new commands: npm link -> run filename
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 
-//* Watch current directory for changes
+//* Wait for 100ms after chokidar finishes event
+const start = debounce(() => {
+  console.log('STARTING USERS PROGRAM');
+}, 100);
+
+//* chokidar watches current directory for changes with events
 chokidar
   .watch('.')
-  .on('add', () => console.log('File Added'))
+  .on('add', () => start)
   .on('change', () => console.log('File Changed'))
   .on('unlink', () => console.log('File Unlinked'));
